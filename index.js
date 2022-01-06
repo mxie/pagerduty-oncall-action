f51a4d75ba854aa23453da7b7fbbcc794c2c8dd9
Melissa Xie <mxie@github.com>
2022-01-05 20:24:05 -0500

    Wrap schedule IDs in an array
    There's an array check somewhere in the API wrapper to make sure the
    query params are constructed appropriately.

--- index.js
+++ index.js
@@ -6,19 +6,20 @@ async function run() {
   const scheduleId = core.getInput("schedule-id");
   const pdClient = pd.api({ token: pdToken });
 
-  pdClient.get('/oncalls', { params: { "schedule_ids[]": scheduleId, "limit": 1 } })
-  .then(({resource}) => {
-    if (resource.length > 0) {
-      person = resource[0]["user"]["summary"]
-      core.info(`🎉 On-call person found: ${person}`);
-      core.setOutput("person", person);
-    } else {
-      core.setFailed("❓ No one is on the schedule");
-    }
-  })
-  .catch((error) => {
-    core.setFailed(`❌ Unable to fetch on-call data: ${error}`);
-  });
+  pdClient
+    .get("/oncalls", { params: { "schedule_ids[]": [scheduleId], limit: 1 } })
+    .then(({ resource }) => {
+      if (resource.length > 0) {
+        person = resource[0]["user"]["summary"];
+        core.info(`🎉 On-call person found: ${person}`);
+        core.setOutput("person", person);
+      } else {
+        core.setFailed("❓ No one is on the schedule");
+      }
+    })
+    .catch((error) => {
+      core.setFailed(`❌ Unable to fetch on-call data: ${error}`);
+    });
 }
 
 run();
